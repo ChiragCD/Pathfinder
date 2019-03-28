4e27079ee4f1d99e3c9e774b7f2a50379c56e441
ChiragCD <Chirag8CD@gmail.com>
2019-03-28 14:12:37 +0530

    Add Path

--- basic/pg.py
+++ basic/pg.py
@@ -6,12 +6,26 @@ pygame.display.set_caption("PATHFINDER")
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)               ## Redundant
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
 def show_map(map, considered_points):
 
     for i in range(len(map)):
         for j in range(len(map)):
             if((i, j) in considered_points):
+                pygame.draw.rect(screen, RED, ((i * 5), (j * 5), 5, 5))
+            elif(map[i][j]):
+                pygame.draw.rect(screen, WHITE, ((i * 5), (j * 5), 5, 5))
+    
+    pygame.display.flip()
+
+def show_path(map, considered_points, path):
+
+    for i in range(len(map)):
+        for j in range(len(map)):
+            if((i, j) in path):
+                pygame.draw.rect(screen, GREEN, ((i * 5), (j * 5), 5, 5))
+            elif((i, j) in considered_points):
                 pygame.draw.rect(screen, RED, ((i * 5), (j * 5), 5, 5))
             elif(map[i][j]):
                 pygame.draw.rect(screen, WHITE, ((i * 5), (j * 5), 5, 5))
